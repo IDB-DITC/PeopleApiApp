faf75e70e9e3c0b39e0629530ee906d5fee280ce
idbditc@gmail.com <idbditc@gmail.com>
2024-01-25 17:11:29 +0600

    Add project files.

---
+++ PeopleApi/Program.cs
@@ -0,0 +1,46 @@
+
+using Microsoft.EntityFrameworkCore;
+using PeopleApp.Models;
+
+namespace PeopleApi
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
+
+			// Add services to the container.
+			builder.Services.AddDbContext<PersonContext>(opt =>
+			{
+				opt.UseSqlServer("server = .; database =ugdvsygvu; trusted_connection =true; trust server certificate = true;");
+			});
+
+
+
+
+			builder.Services.AddControllers();
+			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+			builder.Services.AddEndpointsApiExplorer();
+			builder.Services.AddSwaggerGen();
+
+			var app = builder.Build();
+
+			// Configure the HTTP request pipeline.
+			if (app.Environment.IsDevelopment())
+			{
+				app.UseSwagger();
+				app.UseSwaggerUI();
+			}
+
+			app.UseHttpsRedirection();
+
+			app.UseAuthorization();
+
+
+			app.MapControllers();
+
+			app.Run();
+		}
+	}
+}
---
+++ PeopleApp.Models/PersonContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeopleApp.Models
+{
+	public class PersonContext:DbContext
+	{
+        public DbSet<Person> People { get; set; }
+        public PersonContext(DbContextOptions options):base(options)
+        {
+            
+        }
+    }
+}
